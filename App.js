72c464d9a9f76182a137c91de76127ea6b5cce2b
HimangsuBorah <himangsuborah255@gmail.com>
2023-07-22 23:40:11 +0530

    episode-02

--- App.js
+++ App.js
@@ -6,6 +6,8 @@
 //   },
 //   "Hello world from react"
 // );
+import React from "react";
+import  ReactDOM  from "react-dom/client";
 const parent = React.createElement("div", { id: "parent" }, React.createElement("div", { id: "child" }, [React.createElement("h1", {}, " I am h1"), React.createElement("h2", {}, " I am h2")]))
 console.log(parent);
 const root = ReactDOM.createRoot(document.getElementById("root"));
